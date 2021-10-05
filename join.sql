22521653703de0e4f98a6259db75b32171979bf6
Alexander <alokdev@mail.ru>
2021-10-05 22:27:28 +0300

    add join.sql

---
+++ join.sql
@@ -0,0 +1,64 @@
+-- 1 количество исполнителей в каждом жанре;
+select genser.title, count(*) from performers 
+join genser_perfomers on performers.id = genser_perfomers.perfomers_id
+join genser on genser_perfomers.genser_id = genser.id
+GROUP by genser.title;
+
+-- 2 количество треков, вошедших в альбомы 2019-2020 годов;
+SELECT count(*) FROM traks 
+JOIN albums ON traks.albums_id = albums.id 
+where year BETWEEN 2019 and 2020;
+
+-- 3 средняя продолжительность треков по каждому альбому;
+select albums.title, AVG(duration) FROM traks 
+JOIN albums ON traks.albums_id = albums.id 
+GROUP by albums.title;
+
+-- 4 все исполнители, которые не выпустили альбомы в 2020 году;
+select performers.title from performers 
+join perfomers_albums on performers.id = perfomers_albums.performers_id 
+join albums on perfomers_albums.albums_id = albums.id 
+where year != 2020;
+
+-- 5 названия сборников, в которых присутствует конкретный исполнитель (выберите сами);
+select collection.title from collection 
+join collectin_trak on collection.id = collectin_trak.collection_id 
+join traks on collectin_trak.trak_id = traks.id 
+join albums on traks.albums_id = albums.id
+join perfomers_albums on albums.id = perfomers_albums.albums_id
+join performers on perfomers_albums.performers_id = performers.id 
+where performers.id = 3;
+
+-- 6 название альбомов, в которых присутствуют исполнители более 1 жанра - NO
+select albums.title from albums
+join perfomers_albums on albums.id = perfomers_albums.albums_id
+join performers on perfomers_albums.performers_id = performers.id
+join genser_perfomers on performers.id = genser_perfomers.perfomers_id
+join genser on genser_perfomers.genser_id = genser.id
+GROUP by albums.title
+HAVING count(albums.title) > 2;
+
+
+-- 7 наименование треков, которые не входят в сборники - NO
+select id, title from traks
+join collectin_trak on traks.id = collectin_trak.trak_id
+where not id;
+
+-- 8 исполнителя(-ей), написавшего самый короткий по продолжительности трек - NO
+select performers.title from performers
+
+
+-- 9 название альбомов, содержащих наименьшее количество треков
+select albums.title from albums
+join traks on albums.id = traks.albums_id
+GROUP BY albums.title
+ORDER BY COUNT(*) ASC;
+
+
+
+
+
+
+
+
+
